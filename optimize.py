53c1c4c85e25740f26eb7c9ff0c021aa7de58221
evanprs <evan.prs@gmail.com>
2016-05-25 18:50:58 -0700

    multiple tries, pickling results

--- optimize.py
+++ optimize.py
@@ -5,9 +5,10 @@ from random import random
 import pickle
 
 
-THICKNESS = 6.35/2  # 1/4 inch in mm
-TARGET = np.array([0.22,0.44,0.528,0.66,0.88,1.1])*1000
-METHOD = 'basinhopping' # options: simplex, basinhopping
+THICKNESS = 6.35  # 1/4 inch in mm
+TARGET = np.array([.5,1,1.2,1.5,2,2.5])*440
+METHOD = 'simplex' # options: simplex, basinhopping
+GRADE = 'course' # options: course, fine
 
 def evalFitness(flatpts, target, crosspenalty=100.0*1000):
     """
@@ -27,11 +28,15 @@ def evalFitness(flatpts, target, crosspenalty=100.0*1000):
     pts = (x, y)
     n_freq = len(target)
     try:
-        s = make_shape(pts, max_output_len=50)
+        if GRADE == 'course':
+            s = make_shape(pts, max_output_len=50)
+        else:
+            s = make_shape(pts, max_output_len=100)
         fq, _, _ = find_eigenmodes(s, THICKNESS)
         fit = fitness(fq[:n_freq], target)
         print(fit)
         fits.append(fit)
+        fqs.append(fq)
         return fit
     except ValueError:
         # if you give a constant value, the algorithm thinks it's finished
@@ -51,13 +56,17 @@ def findOptimumCurve(target, c0=None):
         optpts (tuple): points (x,y) defining optimized curve
     """
     if c0 == None:
-        _, c0 = make_random_shape(10, scale=150, circ=True)
+        _, c0 = make_random_shape(7, scale=150, circ=True)
     x, y = c0
     flatpts = np.append(x, y)
     
     if METHOD == 'simplex':
-        retvals = fmin(lambda pts: evalFitness(pts, target), flatpts, disp=True,
-                     ftol=0.1, callback = print, retall=True)
+        if GRADE == 'course':
+            retvals = fmin(lambda pts: evalFitness(pts, target), flatpts, 
+                disp=True, xtol = 1.0, ftol=1.0, retall=True, maxiter=300)
+        else:
+            retvals = fmin(lambda pts: evalFitness(pts, target), flatpts, 
+                disp=True, xtol = .1, ftol=.1, retall=True, maxiter=300)
     elif METHOD == 'basinhopping':
         def test(f_new, x_new, f_old, x_old):
             c = (x_new[:len(x_new) // 2], x_new[len(x_new) // 2:])
@@ -74,19 +83,34 @@ def findOptimumCurve(target, c0=None):
     labels = ['xopt','allvecs']
     retdict = dict(zip(labels,retvals))  # automatically ignores allvecs if absent
     retdict['fits'] = fits
-    pickle.dump(retdict, open('vals.p','wb')) # TODO - account for overwriting
+    retdict['fqs'] = fqs
     
     outpts = retvals[0]
     x = outpts[:len(outpts) // 2]
     y = outpts[len(outpts) // 2:]
     optpts = (x, y)
-    return optpts
+    print(optpts)
+    
+    retdict['optpts'] = optpts # for redundancy 
+    retdict['target'] = target
+    retdict['c0'] = c0
+    
+    pickle.dump(retdict, open('vals.p','wb')) # TODO - account for overwriting
+    return optpts, retdict
 
 
 
 if __name__ == '__main__':
-    fits = [] # TODO - crude, fix this 
-    optpts = findOptimumCurve(TARGET)
-    print(optpts)
-    # curve = make_shape(optpts, max_output_len=50)
-    # print(find_eigenmodes(curve, THICKNESS, showshape=True)[0])
+    
+    attempts = []
+    targets = [TARGET * 2**(n/12.0) for n in range(13)]
+    for trg in targets:
+        i = 0
+        while i < 5:  # try 10 times
+            fits, fqs = [], [] # TODO - crude, fix this 
+            optpts, retdict = findOptimumCurve(trg)
+            attempts.append(retdict)
+            pickle.dump(attempts, open('attempts.p','wb'))
+            i += 1
+    
+    
